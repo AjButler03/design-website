81e3507c7005655329ad6d02bfa788011085beca
Andrew Butler <158104471+AjButler03@users.noreply.github.com>
2024-06-16 23:10:12 -0500

    Starting work
    Tried to create a footer that defaults to bottom of page; doesn't work currently, but it's 11pm and I want to go to bed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    <header>
-      <p>Hello World!</p>
-    </header>
+      <header>
+        <p>Hello World!</p>
+      </header>
+
+      <footer>
+        <p>&copy; 2024</p>
+      </footer>
     </div>
   );
 }
